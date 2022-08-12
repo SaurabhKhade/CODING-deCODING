18f692d3cf78c092128547f7093449bfd747c9cd
Saurabh Khade <khadesaurabh056@gmail.com>
2022-08-12 11:25:00 +0530

    two sum problem complexity reduced to O(n)

--- Easy/1. Two Sum.cpp
+++ Easy/1. Two Sum.cpp
@@ -1,15 +1,14 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-	vector<int> output;
-    for(int i = 0; i < nums.size(); i++){
-        for(int j = i+1; j < nums.size(); j++){
-            if(nums[i] + nums[j] == target){
-                output.push_back(i);
-                output.push_back(j);
+        unordered_map<int,int> map;
+        for(int i=0; i<nums.size(); i++) {
+            int required = target-nums[i];
+            if(map.find(required) != map.end()) {
+                return {map[required],i};
             }
+            map[nums[i]] = i;
         }
+        return {-1,-1};
     }
-    return output;
-}
 };
