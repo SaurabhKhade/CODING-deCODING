8d457286b0c95c591bbfcca9e0f245d3b1ab90ad
Saurabh Khade <khadesaurabh056@gmail.com>
2022-09-30 16:32:28 +0530

    Leetcode: problem 162 optimized

--- LeetCode/Medium/162. Find Peak Element.cpp
+++ LeetCode/Medium/162. Find Peak Element.cpp
@@ -3,26 +3,12 @@ public:
     int findPeakElement(vector<int>& nums) {
         int n = nums.size();
         int low = 0, high = n - 1;
-        while(low != high) {
+        while(low < high) {
             int mid = (low+high)/2;
-            if(mid==low) {
-                if(nums[mid]>nums[mid+1]) 
-                    return mid;
+            if(nums[mid] < nums[mid+1])
                 low = mid+1;
-            }
-            else if(mid==high) {
-                if(nums[mid]>nums[mid-1]) 
-                    return mid;
-                high = mid-1;
-            } 
-            else {
-                if(nums[mid]>nums[mid-1] && nums[mid]>nums[mid+1]) 
-                    return mid;
-                else if(nums[mid]<nums[mid-1]) 
-                    high = mid-1;
-                else
-                    low = mid+1;
-            }
+            else
+                high = mid;
         }
         return low;
     }
