84f0e0be48d358aca8af651339efee91163062c3
Saurabh Khade <khadesaurabh056@gmail.com>
2022-10-23 20:11:28 +0530

    Problem optimized

--- LeetCode/Medium/90. Subsets II.cpp
+++ LeetCode/Medium/90. Subsets II.cpp
@@ -1,38 +1,20 @@
 class Solution {
-    bool isEqual(vector<int> &v1, vector<int> &v2) {
-        if(v1.size()!=v2.size()) return false;
-        for(int i=0; i<v1.size(); i++) {
-            if(v1[i] != v2[i]) {
-                return false;
-            }
-        }
-        return true;
-    }
-    bool isNotFound(vector<vector<int>> &ans, vector<int> &v) {
-        for(auto el:ans) {
-            if(isEqual(el,v)) {
-                return false;
-            }
-        }
-        return true;
-    }
-    void iterate(vector<vector<int>> &ans, vector<int> v, vector<int>& nums, int i) {
-        if(i==nums.size()) {
-            sort(v.begin(), v.end());
-            if(isNotFound(ans, v)) {
-                ans.push_back(v);
-            }
-        }   
-        else {
-            iterate(ans, v, nums, i+1);
-            v.push_back(nums[i]);
-            iterate(ans, v, nums, i+1);
+    void iterate(vector<vector<int>> &ans, vector<int> &v, vector<int>& nums, int i) {
+        ans.push_back(v);
+        
+        for(int j=i; j<nums.size(); j++) {
+            if(j>i && nums[j]==nums[j-1]) continue;
+            v.push_back(nums[j]);
+            iterate(ans,v,nums,j+1);
+            v.pop_back();
         }
     }
 public:
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
-       vector<vector<int>> ans;
-       iterate(ans, {}, nums, 0);
+        sort(nums.begin(), nums.end());
+        vector<vector<int>> ans;
+        vector<int> v;
+        iterate(ans, v, nums, 0);
         return ans;
     }
 };
