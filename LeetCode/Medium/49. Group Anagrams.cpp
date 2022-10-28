596c122c4ea28f90d78290dac3fc8609cfa9b958
Saurabh Khade <khadesaurabh056@gmail.com>
2022-10-28 21:39:20 +0530

    Leetcode: Problem of the day, Oct 28, 2022

--- LeetCode/Medium/49. Group Anagrams.cpp
+++ LeetCode/Medium/49. Group Anagrams.cpp
@@ -2,14 +2,14 @@ class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
         unordered_map<string,vector<string>> map;
-        for(auto &el:strs) {
-            string tmp = el;
-            sort(tmp.begin(), tmp.end());
-            map[tmp].push_back(el);
+        for(auto &str:strs) {
+            string str2 = str;
+            sort(str.begin(),str.end());
+            map[str].push_back(str2);
         }
-        vector<vector<string>>ans;
-        for(auto &pair:map) {
-            ans.push_back(pair.second);
+        vector<vector<string>> ans;
+        for(auto &v:map) {
+            ans.push_back(v.second);
         }
         return ans;
     }
