d6c849c244921dbc630b4f47224c2021836b9277
Saurabh Khade <khadesaurabh056@gmail.com>
2022-10-31 23:13:04 +0530

    Leetcode: 2 problems optimised

--- LeetCode/Easy/733. Flood Fill.cpp
+++ LeetCode/Easy/733. Flood Fill.cpp
@@ -1,24 +1,31 @@
 class Solution {
-    int m, n, toChange;
-    void fillColor(vector<vector<int>>& image, vector<vector<int>>& visited, int color, int sr, int sc) {
-        image[sr][sc] = color;
-        visited[sr][sc] = 1;
-        if(sr>0 && image[sr-1][sc] == toChange && !visited[sr-1][sc]) 
-            fillColor(image, visited, color, sr-1, sc);
-        if(sc>0 && image[sr][sc-1] == toChange && !visited[sr][sc-1]) 
-            fillColor(image, visited, color, sr, sc-1);
-        if(sr<m-1 && image[sr+1][sc] == toChange && !visited[sr+1][sc]) 
-            fillColor(image, visited, color, sr+1, sc);
-        if(sc<n-1 && image[sr][sc+1] == toChange && !visited[sr][sc+1])
-            fillColor(image, visited, color, sr, sc+1);
-    }
 public:
-    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
-        m = image.size();
-        n = image[0].size();
-        toChange = image[sr][sc];
-        vector<vector<int>> visited(m, vector<int>(n,0));
-        fillColor(image, visited, color, sr, sc);
-        return image;
+    vector<vector<int>> floodFill(vector<vector<int>>& img, int sr, int sc, int color) {
+        queue<pair<int,int>> q;
+        vector<vector<int>> visited(img.size(), vector<int> (img[0].size(), 0));
+        
+        q.push({sr,sc});
+        int old = img[sr][sc];
+        img[sr][sc] = color;
+        visited[sr][sc] = 1;
+        
+        int xCoords[] = {-1, 0, 1, 0};
+        int yCoords[] = {0, -1, 0, 1};
+        
+        while(!q.empty()) {
+            int x = q.front().first;
+            int y = q.front().second;
+            q.pop();
+            
+            for(int i=0; i<4; i++) {
+                int x2 = x+xCoords[i], y2 = y+yCoords[i];
+                if(x2>=0 && x2<img.size() && y2>=0 && y2<img[0].size() && !visited[x2][y2] && img[x2][y2]==old) {
+                    visited[x2][y2] = 1;
+                    img[x2][y2] = color;
+                    q.push({x2,y2});
+                }
+            }
+        }
+        return img;
     }
 };
--- LeetCode/Medium/200. Number of Islands.cpp
+++ LeetCode/Medium/200. Number of Islands.cpp
@@ -1,29 +1,35 @@
 class Solution {
-    void removeLand(vector<vector<char>>& grid, int i, int j) {
-        // if cell is invalid or has char '0' return
-        if(i<0 || i>=grid.size() || j<0 || j>=grid[0].size() || grid[i][j] == '0') return;
-        
-        // set cell to '0' and recursively check same for its adjacent cells
+    int m,n;
+    
+    void dfs(vector<vector<char>>& grid, int i, int j, vector<vector<int>> &visited, vector<int> &xCord, vector<int> &yCord) {
         grid[i][j] = '0';
-        removeLand(grid,i-1,j);
-        removeLand(grid,i+1,j);
-        removeLand(grid,i,j-1);
-        removeLand(grid,i,j+1);
+        visited[i][j] = 1;
+        
+        for(int k=0; k<4; k++) {
+            int x2 = i+xCord[k], y2 = j+yCord[k];
+            if(x2>=0 && x2<grid.size() && y2>=0 && y2<grid[0].size() && !visited[x2][y2] && grid[x2][y2]=='1') {
+                dfs(grid, x2, y2, visited, xCord, yCord);
+            }
+        }
     }
 public:
     int numIslands(vector<vector<char>>& grid) {
+        m = grid.size();
+        n = grid[0].size();
+        vector<vector<int>> visited(m, vector<int>(n,0));
         int cnt = 0;
+        vector<int> xCord = {-1, 0, 1, 0};
+        vector<int> yCord = {0, -1, 0, 1};
         
-        // traverse throgh all cells
-        for(int i=0; i<grid.size(); i++) {
-            for(int j=0; j<grid[0].size(); j++) {
-                // if cell is 1 increase count and remove all adjacent 1 and there adjacent 1 and so on...
+        for(int i=0; i<m; i++) {
+            for(int j=0; j<n; j++) {
                 if(grid[i][j]=='1') {
                     cnt++;
-                    removeLand(grid,i,j);
+                    dfs(grid, i, j, visited, xCord, yCord);
                 }
             }
         }
+        
         return cnt;
     }
 };
