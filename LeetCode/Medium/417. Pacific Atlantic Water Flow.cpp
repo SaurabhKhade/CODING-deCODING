b12db463e7a6a95297ee4d0a249d97f1b3edf94a
Saurabh Khade <khadesaurabh056@gmail.com>
2022-11-06 01:31:36 +0530

    Leetcode: 1 problem optimised

--- LeetCode/Medium/417. Pacific Atlantic Water Flow.cpp
+++ LeetCode/Medium/417. Pacific Atlantic Water Flow.cpp
@@ -1,44 +1,45 @@
 class Solution {
-public:
     int m,n;
+    vector<pair<int,int>> moves = {{-1,0},{0,1},{1,0},{0,-1}};
     
-    bool s(vector<vector<bool>>& ocean, int i, int j, vector<vector<int>>& ht){
+public:
+    vector<vector<int>> pacificAtlantic(vector<vector<int>>& heights) {
+        m = heights.size(), n = heights[0].size();
+        vector<vector<int>> pacific(m, vector<int>(n)), atlantic(m, vector<int>(n));
+        vector<vector<int>> ans;
         
-        if (i<0 || j<0 || i==m || j==n || ht[i][j]==100004) return false;
-        if (ocean[i][j]) return true;
+        for(int i=0; i<m; i++) {
+            dfs(heights, pacific, i, 0);
+            dfs(heights, atlantic, i, n-1);
+        }
         
-        int k = ht[i][j];
-        ht[i][j]=100004;
-        bool zz = false;
-        if (i>0 && ht[i-1][j]<=k)   zz = zz || s(ocean,i-1,j,ht);
-        if (j>0 && ht[i][j-1]<=k)   zz = zz || s(ocean,i,j-1,ht);
-        if (i<m-1 && ht[i+1][j]<=k) zz = zz || s(ocean,i+1,j,ht);
-        if (j<n-1 && ht[i][j+1]<=k) zz = zz || s(ocean,i,j+1,ht);
+        for(int i=0; i<n; i++) {
+            dfs(heights, pacific, 0, i);
+            dfs(heights, atlantic, m-1, i);
+        }
         
-        ocean[i][j]=zz;
-        ht[i][j]=k;
-        return zz;
+        for(int i=0; i<m; i++) {
+            for(int j=0; j<n; j++) {
+                if(pacific[i][j] && atlantic[i][j]) {
+                    ans.push_back({i,j});
+                }
+            }
+        }
         
+        return ans;
     }
     
-    vector<vector<int>> pacificAtlantic(vector<vector<int>>& ht) {
-        m = ht.size();
-        n = ht[0].size();
-        vector<vector<bool>> pac(m, vector<bool> (n,false));
-        vector<vector<bool>> atl(m, vector<bool> (n,false));
-        for (int i=0; i<m; i++){
-            pac[i][0]=true;
-            atl[i][n-1]=true;
-        }
-        for (int i=0; i<n; i++){
-            pac[0][i]=true;
-            atl[m-1][i]=true;
-        }
-        vector<vector<int>> res;
-        for (int i=0; i<m; i++){
-            for (int j=0; j<n; j++){
-                if (s(pac,i,j,ht) && s(atl,i,j,ht)) res.push_back({i,j});
+    void dfs(vector<vector<int>>& heights, vector<vector<int>> &ocean, int i, int j) {
+        ocean[i][j] = 1;
+        
+        for(auto [x,y]:moves) {
+            x += i, y += j;
+            if(x<0 || y<0 || x>=m || y>=n) {
+                continue;
+            } 
+            if(ocean[x][y] == 0 && heights[x][y] >= heights[i][j]) {
+               dfs(heights, ocean, x, y);
             }
-        }return res;
+        }
     }
 };
