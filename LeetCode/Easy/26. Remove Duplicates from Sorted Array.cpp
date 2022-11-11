b0931dc2bd6816d904eeb52f27bf663eded6213b
Saurabh Khade <khadesaurabh056@gmail.com>
2022-11-11 22:03:27 +0530

    Leetcode: Problem of the day, Nov 11, 2022

--- LeetCode/Easy/26. Remove Duplicates from Sorted Array.cpp
+++ LeetCode/Easy/26. Remove Duplicates from Sorted Array.cpp
@@ -1,9 +1,9 @@
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
-        int slow = 0, fast = 1;
-        for(fast; fast<nums.size(); fast++) {
-            if(nums[fast]!=nums[slow]) {
+        int slow=0, fast =1;
+        for(; fast<nums.size(); fast++) {
+            if(nums[slow]!=nums[fast]) {
                 nums[++slow] = nums[fast];
             }
         }
